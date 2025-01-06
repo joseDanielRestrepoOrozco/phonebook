9e2eecd5e9ae60944f0e76ff4b964e35e95a0f23
jose <jose.resoro4789@gmail.com>
2025-01-06 12:37:47 -0500

    uibuild

--- models/person.js
+++ models/person.js
@@ -14,8 +14,21 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: v => {
+        return /^\d{2,3}-\d{6,8}$/.test(v)
+      },
+      message: props => `${props.value} no es un número de teléfono válido!`,
+    },
+    required: true,
+  },
 })
 
 personSchema.set('toJSON', {
